29fcf096907efc4b5764f61e42fc0911d4ef51ab
mrcopycat25 <mrcopycat25@gmail.com>
2024-04-03 16:55:10 +0200

    selection

--- Scripts/Units/FriendlyUnit.gd
+++ Scripts/Units/FriendlyUnit.gd
@@ -1,10 +1,22 @@
 extends "res://Scripts/Units/Unit.gd"
-
-var selected = true
+var selected = false
+var mouseOver = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-func _physics_process(delta):
-	 pass
+
+func _on_Area2D_mouse_entered():
+	mouseOver=true
+
+
+func _on_Area2D_mouse_exited():
+	mouseOver=false
+
+func _input(event):
+	if event is InputEventMouseButton && event.get_button_index()==1 && mouseOver==true:
+		selected=true
+		
+	if event is InputEventMouseButton && event.get_button_index()==1 && mouseOver==false:
+		selected=false
