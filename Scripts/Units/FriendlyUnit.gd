39feb97bc61147a201dc5d7e6de52b470a50b4cd
mrcopycat25 <mrcopycat25@gmail.com>
2024-04-10 17:11:56 +0200

    mass selection

--- Scripts/Units/FriendlyUnit.gd
+++ Scripts/Units/FriendlyUnit.gd
@@ -23,6 +23,9 @@ func _on_Area2D_mouse_entered():
 func _on_Area2D_mouse_exited():
 	mouseOver=false
 
+
+
+
 func _input(event):
 	if event is InputEventMouseButton && event.get_button_index()==1 && mouseOver==true:
 		selected=true
