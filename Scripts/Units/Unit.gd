f03f61d2cc1978e2969e61ac47c0db2f95645a71
Pain <ebbemandsen@gmail.com>
2024-04-15 09:26:41 +0200

    sounds

--- Scripts/Units/Unit.gd
+++ Scripts/Units/Unit.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-# Constants
-
 # Movement
 var velocity = Vector2.ZERO
 var acceleration = Vector2.ZERO
@@ -18,9 +16,21 @@ func _ready():
 	pass
 
 func _physics_process(delta):
+	setHealth(health - 1)
 	pass
 
 func stopOnCollision():
 	if is_on_wall():
 		acceleration.x = 0
 		velocity.x = 0
+
+func setHealth(newHealth):
+	health = newHealth
+	if health <= 0:
+		die()
+
+func die():
+	$DeathSound.play()
+	while rotation_degrees < 90:
+		rotate(1)
+	queue_free()
