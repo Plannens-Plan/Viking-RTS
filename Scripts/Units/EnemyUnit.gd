1f216f317e6a6a9078d7279b393804d55058b2d9
SmokeyBBQx <47431313+SmokeyBBQx@users.noreply.github.com>
2024-04-16 11:20:10 +0200

    fix enemy AI updating

--- Scripts/Units/EnemyUnit.gd
+++ Scripts/Units/EnemyUnit.gd
@@ -1,17 +1,26 @@
 extends "res://Scripts/Units/Unit.gd"
 
-var closestDistance = 0
+var closestDistance = 1000
 var closestUnit = null
 
-func TargettingFriendly():
+func _physics_process(delta):
+	print(closestDistance)
+	print(closestUnit)
+	
+	TargetFriendly()
+	Attack()
+
+func TargetFriendly():
 	for body in $DetectArea.get_overlapping_bodies():
 			if body.is_in_group("friendlyUnit"):
 				var distance = position.distance_to(body.position)
+				print(distance)
 				if distance < closestDistance:
 					closestDistance = distance
 					closestUnit = body
-					set_target_location(closestUnit.position)
 
-func _physics_process(delta):
-	Attack()
-	pass
+	if not closestUnit == null:
+		if is_instance_valid(closestUnit):
+			set_target_location(closestUnit.position)
+		else:
+			closestDistance = 1000
