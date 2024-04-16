5aa19899e1a93f3209b70ecb7dde5b5b90b172b7
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2024-04-16 13:01:10 +0200

    save and load units
    added saving for units
    
    added variable to unit and edited miner and friendly unit

--- Scripts/Units/FriendlySpearman.gd
+++ Scripts/Units/FriendlySpearman.gd
@@ -1,10 +1,12 @@
 extends "res://Scripts/Units/FriendlyUnit.gd"
 
 func _ready():
-	health = 75
-	moveSpeed = 75
-	attackDamage = 35
-	attackSpeed = 2
+	if newunit:
+		health = 75
+		maxHealth=75
+		moveSpeed = 75
+		attackDamage = 35
+		attackSpeed = 2
 	updateElements()
 
 func _physics_process(delta):
