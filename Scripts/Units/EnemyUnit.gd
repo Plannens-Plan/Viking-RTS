ef19638440a8166b1693d1440d2e24788d2bd516
Ebb <ebbemandsen@gmail.com>
2024-04-16 12:07:39 +0200

    Update EnemyUnit.gd

--- Scripts/Units/EnemyUnit.gd
+++ Scripts/Units/EnemyUnit.gd
@@ -4,9 +4,6 @@ var closestDistance = 1000
 var closestUnit = null
 
 func _physics_process(delta):
-	print(closestDistance)
-	print(closestUnit)
-	
 	TargetFriendly()
 	Attack()
 
@@ -14,13 +11,10 @@ func TargetFriendly():
 	for body in $DetectArea.get_overlapping_bodies():
 			if body.is_in_group("friendlyUnit"):
 				var distance = position.distance_to(body.position)
-				print(distance)
 				if distance < closestDistance:
 					closestDistance = distance
 					closestUnit = body
 
-func _physics_process(delta):
-	Attack()
 	if not closestUnit == null:
 		if is_instance_valid(closestUnit):
 			set_target_location(closestUnit.position)
