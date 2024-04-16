fe3b3a3cddfea8813583002391374a8ede2b8e69
Ebb <ebbemandsen@gmail.com>
2024-04-16 16:36:59 +0200

    outline revamp

--- Scripts/Units/FriendlyUnit.gd
+++ Scripts/Units/FriendlyUnit.gd
@@ -7,13 +7,11 @@ func _ready():
 func _physics_process(delta):
 	Attack()
 	if !selected:
-		$Sprite.material = null
+		$Sprite.material.set_shader_param("hide", true)
 	else:
-		# Give outline
-		$Sprite.material = load("res://Assets/Materials/Outline.tres")
+		$Sprite.material.set_shader_param("hide", false)
 		# Fade in to show health bar
 		$HealthBar.modulate.a = lerp($HealthBar.modulate.a, 1, healthBarFadeSpeed)
-
 
 func _input(event):
 	if event is InputEventMouseButton && event.get_button_index() == 1:
