3a55baf99dcce0cb494f7c4ecefb10fa61cfb2d6
Ebb <ebbemandsen@gmail.com>
2024-04-17 22:20:21 +0200

    formatting

--- Scripts/Units/Unit.gd
+++ Scripts/Units/Unit.gd
@@ -33,10 +33,14 @@ var healthBarFadeSpeed = 0.1
 var healthBarProgressSpeed = 0.1
 
 var rng = RandomNumberGenerator.new()
-# rng block number
+# RNG block number
 var blockNumber
 
+# The sound a unit makes when attacking
 var attackSound
+
+# The signal a unit sends when it dies
+signal dead_soldier
 
 func _ready():
 	updateElements()
@@ -115,7 +119,6 @@ func setHealth(newHealth, canBeBlocked):
 		die()
 
 
-signal dead_soldier
 func die():
 	var deathEffectInst = death_effect.instance()
 	deathEffectInst.unitSprite = $Sprite.texture
