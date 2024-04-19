7a9d7f7caa963aee0456d70b26d657f04a170b72
Ebb <ebbemandsen@gmail.com>
2024-04-19 14:22:56 +0200

    trading fasdhagf

--- Scripts/Structures/TradingPost.gd
+++ Scripts/Structures/TradingPost.gd
@@ -5,11 +5,9 @@ var scene
 
 func _ready():
 	scene = get_tree().current_scene
-	pass
 
-func _process(delta):
-	print(scene.get_node("GUI").get_node("TradeMap").visible)
-
-func _input(event):
-	if mouseOver == true && event.is_action_released("leftClick"):
-			scene.get_node("GUI").get_node("TradeMap").show()
+func _physics_process(delta):
+	if selected:
+		scene.get_node("GUI").get_node("TradeMap").show()
+	else:
+		scene.get_node("GUI").get_node("TradeMap").hide()
