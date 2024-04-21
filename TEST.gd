665d4019aec88b1ce4f8ff45c9ab06c3a253f215
mrcopycat25 <mrcopycat25@gmail.com>
2024-04-21 19:05:24 +0200

    shhhhh

--- TEST.gd
+++ TEST.gd
@@ -1,8 +1,6 @@
-extends Control
-
-func _process(delta):
-	print($Background)
-	$Background.get_rect().size.x = get_viewport().size.x
-	$Background.get_rect().size.y = get_viewport().size.y
-	$Background.rect_scale.x = get_viewport().size.x / $Background.texture.get_size().x
-	$Background.rect_scale.y = get_viewport().size.y / $Background.texture.get_size().y
+extends Sprite
+func _input(event):
+	if event.is_action_pressed("Test"):
+		show()
+	if event.is_action_released("Test"):
+		hide()
